0a0f6ceb67de1a7c54e9de126cd5ed225b138f89
miguel <migueldelafuente4@gmail.com>
2023-01-23 18:30:36 +0100

    main example

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ Created on Jan 10, 2023
 
 @author: Miguel
 '''
-from scripts1d.beta_scripts import run_q20_surface
-from tools.helpers import importAndCompile_taurus
+import os
+from scripts1d.beta_scripts import run_q20_surface, run_b20_surface
+from tools.helpers import importAndCompile_taurus, TBME_SUITE
 
-from tools.hamiltonian_maker import TBME_HamiltonianManager
+from tools.hamiltonianMaker import TBME_HamiltonianManager
+from tools.inputs import InputTaurus
 
-
+if not (InputTaurus.PROGRAM in os.listdir()):
+    importAndCompile_taurus()
 
 if __name__ == '__main__':
     
-    exe_ = TBME_HamiltonianManager(1.69, 2, set_com2=True)
+    exe_ = TBME_HamiltonianManager(1.75, 4, set_com2=True)
     exe_.setAndRun_D1Sxml()
     
     # importAndCompile_taurus()
     
     nucleus = [(2, 4), (4, 4)]
-    run_q20_surface(nucleus, q_min=-5, q_max=5, N_max=10)
+    interactions = {(2,4): (4,0, 2.0), (4,4): 'hamil_2'}
+    run_b20_surface(nucleus, q_min=-1., q_max=1., N_max=10)
+    
